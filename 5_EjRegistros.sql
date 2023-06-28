893fcdacba8c823e44ce00af4856dc59c1d02da2
Mordisquitos <mariojm2612@gmail.com>
2023-06-28 19:52:10 +0200

    6,7,8 a medias

--- 5_EjRegistros.sql
+++ 5_EjRegistros.sql
@@ -1,20 +1,21 @@
-USE ej4_tabla;
+USE ej4_tablas;
 TRUNCATE TABLE album;
 TRUNCATE TABLE artist;
 TRUNCATE TABLE genre;
--- SELECT * FROM ej4_tabla.album;
+
 INSERT INTO album(albumName,dateReleased) VALUES 
 ("Album1","2021-10-1"),("Album2","2021-10-2"),("Album3","2021-10-3"),
 ("Album4","2021-10-4"),("Album5","2021-10-5"),("Album6","2021-10-6"),
 ("Album7","2021-10-7"),("Album8","2021-10-8"),("Album9","2021-10-9"),("Album10","2021-10-10");
--- SELECT * FROM ej4_tabla.artist;
+
 INSERT INTO artist(artistName) VALUES 
 ("Elvis"),("Rosalia"),("Rauw Alejandro"),("Blas"),("Quim"),
 ("David"),("Queen"),("Beatles"),("MIKA"),("Mario");
--- SELECT * FROM ej4_tabla.genre;
+
 INSERT INTO genre(genre) VALUES 
 ("rock"),("pop"),("jazz"),("heavy"),("clásico"),
 ("house"),("electronic"),("hardcore"),("trap"),("rap");
-SELECT * FROM ej4_tabla.album;
-SELECT * FROM ej4_tabla.artist;
-SELECT * FROM ej4_tabla.genre;
+
+SELECT * FROM album;
+SELECT * FROM artist;
+SELECT * FROM genre;
---
+++ 8_EjAddressCity.sql
@@ -0,0 +1,70 @@
+DROP DATABASE IF EXISTS ej8_tablas;
+CREATE DATABASE IF NOT EXISTS ej8_tablas;
+
+USE ej8_tablas;
+
+CREATE TABLE address(
+address_id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+address VARCHAR(50) NOT NULL,
+address2 VARCHAR(50),
+district VARCHAR(20) NOT NULL,
+postal_code VARCHAR(10),
+phone VARCHAR(20) NOT NULL,
+location VARCHAR(20) NOT NULL,
+last_update TIMESTAMP NOT NULL
+);
+
+CREATE TABLE city(
+city_id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+city VARCHAR(50) NOT NULL,
+last_update TIMESTAMP NOT NULL
+);
+
+CREATE TABLE country(
+country_id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+country VARCHAR(50) NOT NULL,
+last_update TIMESTAMP NOT NULL
+);
+
+TRUNCATE address;
+TRUNCATE city;
+TRUNCATE country;
+
+INSERT INTO address (address, address2, district, postal_code, phone, location, last_update) VALUES
+('123 Main St', 'Apt 4', 'District 1', '12345', '555-1234', 'Location 1', CURRENT_TIMESTAMP),
+('456 Elm St', NULL, 'District 2', '67890', '555-5678', 'Location 2', CURRENT_TIMESTAMP),
+('789 Oak St', 'Suite 10', 'District 3', '24680', '555-9101', 'Location 3', CURRENT_TIMESTAMP),
+('321 Pine St', NULL, 'District 4', '13579', '555-1112', 'Location 4', CURRENT_TIMESTAMP),
+('654 Maple St', 'Apt 7', 'District 5', '97531', '555-1314', 'Location 5', CURRENT_TIMESTAMP),
+('987 Birch St', 'Unit 3', 'District 6', '46802', '555-1516', 'Location 6', CURRENT_TIMESTAMP),
+('159 Walnut St', NULL, 'District 7', '80246', '555-1718', 'Location 7', CURRENT_TIMESTAMP),
+('753 Chestnut St', 'Suite 5', 'District 8', '62480', '555-1920', 'Location 8', CURRENT_TIMESTAMP),
+('852 Spruce St', 'Apt 2', 'District 9', '20864', '555-2122', 'Location 9', CURRENT_TIMESTAMP),
+('426 Ash St', NULL, 'District 10', '74026', '555-2324', 'Location 10', CURRENT_TIMESTAMP);
+
+INSERT INTO city (city, last_update) VALUES
+('New York', CURRENT_TIMESTAMP),
+('London', CURRENT_TIMESTAMP),
+('Paris', CURRENT_TIMESTAMP),
+('Tokyo', CURRENT_TIMESTAMP),
+('Sydney', CURRENT_TIMESTAMP),
+('Dubai', CURRENT_TIMESTAMP),
+('Rome', CURRENT_TIMESTAMP),
+('Toronto', CURRENT_TIMESTAMP),
+('Moscow', CURRENT_TIMESTAMP),
+('Berlin', CURRENT_TIMESTAMP);
+
+INSERT INTO country (country, last_update) VALUES
+('United States', CURRENT_TIMESTAMP),
+('United Kingdom', CURRENT_TIMESTAMP),
+('France', CURRENT_TIMESTAMP),
+('Japan', CURRENT_TIMESTAMP),
+('Australia', CURRENT_TIMESTAMP),
+('United Arab Emirates', CURRENT_TIMESTAMP),
+('Italy', CURRENT_TIMESTAMP),
+('Canada', CURRENT_TIMESTAMP),
+('Russia', CURRENT_TIMESTAMP),
+('Germany', CURRENT_TIMESTAMP);
+
+SELECT * FROM country
+INNER JOIN city ON ;
